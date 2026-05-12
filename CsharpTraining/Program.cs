98a40fd7cb590df1e14aa930171aa8d2a6efe7b0
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-12 10:34:03 +0400

    Added the shared varibles withinital values

--- CsharpTraining/Program.cs
+++ CsharpTraining/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Shared varibles
+            int accountNumber = 0;
+            double balance = 0;
+            bool isActive= false;
+            char accountType = ' ';
+            bool isEmployed = false;
+            double salary = 0;
+            int creditScore = 0;
+            int age = 0;
+            double deposit = 0;
+            double withdrawl = 0;
+            double annualRate = 0;
+            double avgBalance = 0;
+
+            
         }
     }
 }

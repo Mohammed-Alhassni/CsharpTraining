de7d7020f493b00879595d0c096464656ec4ac2c
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-05-14 15:05:48 +0400

    Created template for the project main menu

--- ClinicManagementSystem/Program.cs
+++ ClinicManagementSystem/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //System Storage: all variable declarations
+
+            //Main Menu: the outer while loop + switch-case
+            int option = -1;
+
+            while (option != 0)
+            {
+                switch (option)
+                {
+                    //Sub - Menus: one while loop per entity(inside the main menu switch-case case)
+                    //Operations: the if-else logic for each CRUD action
+                    case 0:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }

0583708c85d12061d0619f1ec77e813f10acc15a
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-05-16 23:47:00 +0400

    Fixed logical error: when doctor deleted, the counter is not decremented

--- ClinicManagementSystem/Program.cs
+++ ClinicManagementSystem/Program.cs
@@ -217,8 +217,8 @@
                                 case 4:
                                     string targetDeleteDoctor;
                                     Console.Write("Enter Target Doctor Name: "); targetDeleteDoctor = Console.ReadLine();
-                                    if (d1Active && d1Name == targetDeleteDoctor) { d1Name = ""; d1Fee = 0; d1Spec = ""; d1Active = false; Console.Write($"{targetDeleteDoctor} have been deleted."); }
-                                    if (d2Active && d2Name == targetDeleteDoctor) { d2Name = ""; d2Fee = 0; d2Spec = ""; d2Active = false; Console.Write($"{targetDeleteDoctor} have been deleted."); }
+                                    if (d1Active && d1Name == targetDeleteDoctor) { d1Name = ""; d1Fee = 0; d1Spec = ""; d1Active = false; doctorCount--; Console.Write($"{targetDeleteDoctor} have been deleted."); }
+                                    if (d2Active && d2Name == targetDeleteDoctor) { d2Name = ""; d2Fee = 0; d2Spec = ""; d2Active = false; doctorCount--; Console.Write($"{targetDeleteDoctor} have been deleted."); }
                                     else { Console.WriteLine("This Doctor Does not exist "); }
                                     break;
                                 case 0:
